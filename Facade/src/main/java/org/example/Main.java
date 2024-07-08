aea1ad079ca1c19a6aab081cbdc09a7778110075
mygit0001 <68539079+mygit0001@users.noreply.github.com>
2024-07-08 22:04:14 +0530

    Add files via upload

---
+++ Facade/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.example.facade.VideoConversionFacade;
+
+import java.io.File;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        VideoConversionFacade converter = new VideoConversionFacade();
+        File mp4Video = converter.convertVideo("youtubevideo.ogg", "mp4");
+    }
+}
