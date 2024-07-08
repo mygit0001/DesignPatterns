877f05958b2ea59bd749c40afb86823337cfb805
mygit0001 <68539079+mygit0001@users.noreply.github.com>
2024-07-08 21:47:03 +0530

    Add files via upload

---
+++ Singleton/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("If you see the same value, then singleton was reused (yay!)" + "\n" +
+                "If you see different values, then 2 singletons were created (booo!!)" + "\n\n" +
+                "RESULT:" + "\n");
+        Singleton singleton = Singleton.getInstance("BAR");
+        Singleton anotherSingleton = Singleton.getInstance("FOO");
+        System.out.println(singleton.value);
+        System.out.println(anotherSingleton.value);
+    }
+}
