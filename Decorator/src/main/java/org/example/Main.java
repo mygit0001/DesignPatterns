3d5e9a99ffff86bf9a0601dc590c649cee64b837
mygit0001 <68539079+mygit0001@users.noreply.github.com>
2024-07-08 21:29:29 +0530

    Add files via upload

---
+++ Decorator/src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.example.decorators.*;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        String salaryRecords = "Name,Salary\nJohn Smith,100000\nSteven Jobs,912000";
+        DataSourceDecorator encoded = new CompressionDecorator(
+                new EncryptionDecorator(
+                        new FileDataSource("out/OutputDemo.txt")));
+        encoded.writeData(salaryRecords);
+        DataSource plain = new FileDataSource("out/OutputDemo.txt");
+
+        System.out.println("- Input ----------------");
+        System.out.println(salaryRecords);
+        System.out.println("- Encoded --------------");
+        System.out.println(plain.readData());
+        System.out.println("- Decoded --------------");
+        System.out.println(encoded.readData());
+        }
+    }
---
+++ Decorator/src/main/java/org/example/decorators/DataSource.java
@@ -0,0 +1,7 @@
+package org.example.decorators;
+
+public interface DataSource {
+    void writeData(String data);
+
+    String readData();
+}
